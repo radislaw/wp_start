393a08ac9cc9cddd3206211793d62810bc5ab03c
Radislaw <radislaw22@gmail.com>
2016-09-21 12:34:53 +1000

    initial

---
+++ functions.php
@@ -0,0 +1,22 @@
+<?php
+/* enqueue */
+
+/* widgets */
+
+/* thumbnail in posts */
+
+/* customizer */
+
+/* reset thumbnail inline styles */
+
+/* custom posts */
+
+/* custom taxonomy */
+
+/* custom metaboxes */
+
+/* slider from  gallery shordcode */
+
+/* ??? */
+
+?>
